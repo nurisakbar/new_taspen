39c5d39a9c43fb825efadfaa2764f6250ce0e432
Nuris Akbar <nuris.akbar@gmail.com>
2025-10-29 14:12:25 +0700

    Update WelcomeGreetingController.php

--- app/Http/Controllers/WelcomeGreetingController.php
+++ app/Http/Controllers/WelcomeGreetingController.php
@@ -36,8 +36,8 @@ class WelcomeGreetingController extends Controller
         $filename          = "polis-verify/".$pesan->id;
 
         // Build and send WhatsApp via Qontak (template id provided by user)
-        $messageTemplateId = '7451799a-df24-4fa1-9a94-cfb61c851223';
-        // $messageTemplateId = "897edb7a-86a4-44c3-99da-453caa7d7c42";
+        //$messageTemplateId = '7451799a-df24-4fa1-9a94-cfb61c851223';
+        $messageTemplateId = "897edb7a-86a4-44c3-99da-453caa7d7c42";
         $qontakService = app(QontakService::class);
         $waPayload = [
             'to_name' => $validated['nama_peserta'],
